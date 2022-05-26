984d5ba2b2473c8bd021511858376bcc64f6b033
Lucas De alencastro moreira <lde-alen@student.42abudhabi.ae>
2022-05-26 22:12:12 +0400

    Structures added into header + some potential defines

--- includes/minishell.h
+++ includes/minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: asanthos <asanthos@student.42.fr>          +#+  +:+       +#+        */
+/*   By: lde-alen <lde-alen@student.42abudhabi.ae>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/17 12:25:16 by lde-alen          #+#    #+#             */
-/*   Updated: 2022/05/26 12:10:41 by asanthos         ###   ########.fr       */
+/*   Updated: 2022/05/26 22:11:47 by lde-alen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,40 @@
 # include <signal.h>
 # include <stdlib.h>
 
+/**
+ * Setting your vocabulary makes you wiser
+ */
+# define NOTHING	-1
+# define R_REDIR	0
+# define L_REDIR	1
+# define DR_REDIR	2
+# define DL_REDIR	3
+# define PIPE		4
+# define S_QUOTE	5
+# define D_QUOTE	6
+# define CASHMONEY	7
+
+/**
+ * Defining your tools and refining them is the key
+ */
+typedef struct s_cmd
+{
+	char			*command;
+	char			**argument;
+	struct s_cmd	*next;
+}	t_cmd;
+
+typedef struct s_env
+{
+	char			*name;
+	char			*value;
+	struct s_env	*prev;
+	struct s_env	*next;
+}	t_env;
+
+/**
+ * Teaching kinds how to manipulate tools
+ */
 void	shell_prompt(void);
 void	exec_cmd(char *str);
 
