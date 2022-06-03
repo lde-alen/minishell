0a22fcc4f4d1f3475d195287730276a9a0b52dcc
Alka Santhosh <alka.sv54@gmail.com>
2022-06-03 18:43:07 +0400

    echo fixedazzzzz

--- srcs/builtins/ms_echo.c
+++ srcs/builtins/ms_echo.c
@@ -6,20 +6,34 @@
 /*   By: asanthos <asanthos@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/03 12:17:32 by asanthos          #+#    #+#             */
-/*   Updated: 2022/06/03 18:21:35 by asanthos         ###   ########.fr       */
+/*   Updated: 2022/06/03 18:42:34 by asanthos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-int	get_len(t_cmd *cmd_lst, int i)
+size_t	get_len(t_cmd *cmd_lst, int i)
 {
-	int	len;
+	size_t	len;
 
 	len = 0;
 	while (cmd_lst->argument[i])
 	{
 		len++;
+		i++;
+	}
+	return (len);
+}
+
+size_t	args_len(t_cmd *cmd_lst, int i)
+{
+	size_t	len;
+
+	i = 0;
+	len = 0;
+	while (cmd_lst->argument[i])
+	{
+		len += ft_strlen(cmd_lst->argument[i]);
 		i++;
 	}
 	return (len);
@@ -32,11 +46,9 @@ void	ft_join_echo(t_cmd *cmd_lst, int i, int flag)
 
 	str = "";
 	store = i;
-	str = (char *)malloc(sizeof(char) * get_len(cmd_lst, i));
+	str = (char *)malloc(sizeof(char) * (get_len(cmd_lst, i) + args_len(cmd_lst, i)));
 	while (cmd_lst->argument[i])
 	{
-		if (i != store)
-			str = ft_strjoin(str, " ");
 		str = ft_strjoin(str, cmd_lst->argument[i]);
 		str = ft_strjoin(str, " ");
 		i++;
