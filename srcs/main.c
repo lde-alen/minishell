2a456d0e363e61defec7e82d432b9e37a91a03b5
Alka Santhosh <alka.sv54@gmail.com>
2022-06-03 11:12:36 +0400

    edited the main(too many args)

--- srcs/main.c
+++ srcs/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lde-alen <lde-alen@student.42abudhabi.ae>  +#+  +:+       +#+        */
+/*   By: asanthos <asanthos@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/17 12:00:41 by lde-alen          #+#    #+#             */
-/*   Updated: 2022/06/02 19:51:23 by lde-alen         ###   ########.fr       */
+/*   Updated: 2022/06/03 11:11:48 by asanthos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,17 @@
 
 int	main(int ac, char **av, char **env)
 {
-	if (*env == NULL)
+	if (ac == 1)
 	{
-		ft_putendl_fd("Error: ENV = NULL", 2);
-		return (0);
+		if (*env == NULL)
+		{
+			ft_putendl_fd("Error: ENV = NULL", 2);
+			return (0);
+		}
+		else
+			minishell(ac, av, env);
 	}
 	else
-		minishell(ac, av, env);
+		ft_putendl_fd("Too many arguments!!\n\nRun with:\n./minishell", 2);
 	return (0);
 }
