e2f53e528bf9cd98974270707ab06f63d7c6c7e0
Alka <alka.sv54@gmail.com>
2022-06-04 08:15:20 +0400

    fix cd

--- srcs/builtins/ft_cd.c
+++ srcs/builtins/ft_cd.c
@@ -6,7 +6,7 @@
 /*   By: asanthos <asanthos@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/04 04:13:06 by asanthos          #+#    #+#             */
-/*   Updated: 2022/06/04 04:17:37 by asanthos         ###   ########.fr       */
+/*   Updated: 2022/06/04 05:14:16 by asanthos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,16 @@
 
 void ft_cd(t_cmd *cmd_lst)
 {
+    int i;
+
+    i = 0;
+    if (!cmd_lst->argument[0] || cmd_lst->argument[0] == '~')
+    {
+        while (1)
+        {
+            if ()
+        }
+        chdir("Desktop");
+    }
     chdir(cmd_lst->argument[0]);
 }
