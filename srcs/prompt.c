0c6fe2c9bc30001a97ad9af83de6109789a62e7b
Lucas De alencastro moreira <lde-alen@student.42abudhabi.ae>
2022-08-07 22:07:26 +0400

    quotes + redir check done but not norme friendly

--- srcs/prompt.c
+++ srcs/prompt.c
@@ -6,7 +6,7 @@
 /*   By: lde-alen <lde-alen@student.42abudhabi.ae>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/25 09:05:25 by asanthos          #+#    #+#             */
-/*   Updated: 2022/08/07 20:22:11 by lde-alen         ###   ########.fr       */
+/*   Updated: 2022/08/07 22:07:07 by lde-alen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,7 @@ void	shell_prompt(char **env)
 	(void)env;
 	signal(SIGINT, new_prompt);
 	signal(SIGQUIT, SIG_IGN);
+	str = "";
 	while (1)
 	{
 		str = readline("\e[0;37m|🐼| \e[1;35mminishell\e[0;37m$\e[0m ");
