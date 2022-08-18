c6bd730145116b5bb686382fbea5febe74e6a914
Alka <alka.sv54@gmail.com>
2022-08-18 16:18:57 +0400

    mulit pipes almost done

--- srcs/exec_test.c
+++ srcs/exec_test.c
@@ -6,7 +6,7 @@
 /*   By: asanthos <asanthos@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/14 13:25:51 by asanthos          #+#    #+#             */
-/*   Updated: 2022/08/17 16:57:29 by asanthos         ###   ########.fr       */
+/*   Updated: 2022/08/18 16:08:36 by asanthos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,17 +21,7 @@ void	main_child2(t_cmd *cmd_lst, char *path, char **env_kid)
 
 static void	first_child(t_cmd *cmd_lst, int (*fd)[2], char *path, char **env_kid, int i)
 {
-	int	j;
-
-	j = 0;
-	while (cmd_lst != NULL)
-	{
-		if (j != i)
-			close(fd[j][1]);
-		close(fd[j][0]);
-		j++;
-		cmd_lst = cmd_lst->next;
-	}
+	close(fd[i][0]);
 	if (dup2(fd[i][1], STDOUT_FILENO) < 0)
 		perror("dup2");
 	close(fd[i][1]);
@@ -40,80 +30,41 @@ static void	first_child(t_cmd *cmd_lst, int (*fd)[2], char *path, char **env_kid
 	exit(0);
 }
 
-static void	last_child(t_cmd *cmd_lst, int (*fd)[2], char *path, char **env_kid, int i, int len)
+static void	last_child(t_cmd *cmd_lst, int (*fd)[2], char *path, char **env_kid, int i)
 {
-	int	j;
-
-	j = 0;
-	while (j <= len)
-	{
-		if (j != (i - 1))
-			close(fd[j][0]);
-		close(fd[j][1]);
-		j++;
-	}
 	if (dup2(fd[(i - 1)][0], STDIN_FILENO) < 0)
 		perror("dup2ME");
 	close(fd[(i - 1)][0]);
-	ft_printf("%s\n", path);
 	if (execve(path, cmd_lst->argument, env_kid) < 0)
 		perror("Execve problem");
 	exit(0);
 } 
 
 
-void	mid_kid(t_cmd *cmd_lst, int (*fd)[2], char *path, char **env_kid, int i, int len)
+void	mid_kid(t_cmd *cmd_lst, int (*fd)[2], char *path, char **env_kid, int i)
 {
-	int	j;
-
-	j = 0;
-	while (j <= len)
-	{
-		if (j != i)
-			close(fd[j][1]);
-		if (j != (i - 1))
-			close(fd[j][0]);
-		i++;
-	}
-	int b = 0;
-	ft_printf("Mid child\n");
-	while (cmd_lst->argument[b])
-	{
-		ft_printf("%s\n", cmd_lst->argument[b]);
-		b++;
-	}
-	ft_printf("%s\n", cmd_lst->argument[i]);
+	close(fd[i][0]);
 	if (dup2(fd[(i - 1)][0], STDIN_FILENO) < 0)
 		perror("dup2_Mid1");
 	if (dup2(fd[i][1], STDOUT_FILENO) < 0)
 		perror("dup2_mid2");
 	close(fd[(i - 1)][0]);
 	close(fd[i][1]);
-	ft_printf("%s\n", path);
 	if (execve(path, cmd_lst->argument, env_kid) < 0)
 		perror("Execve problem");
 	exit(0);
 }
 
-void	pipe_arr(t_cmd *cmd_lst, int (*fd)[2])
+void	pipe_arr(int (*fd)[2], int i)
 {
-	int		i;
-
-	i = 0;
-	while (cmd_lst != NULL)
+	if (pipe(fd[i]) < 0)
 	{
-		if (pipe(fd[i]) < 0)
+		if (i != 0)
 		{
-			while (i >= 0)
-			{
-				close(fd[i][0]);
-				close(fd[i][1]);
-				i--;
-			}
-			perror("pipe");
+			close(fd[i - 1][0]);
+			close(fd[i - 1][1]);
 		}
-		i++;
-		cmd_lst = cmd_lst->next;
+		perror("pipe");
 	}
 }
 
@@ -127,24 +78,22 @@ void	check_pos(t_env *lst, t_cmd *cmd_lst, int (*fd)[2], int len, int i)
 	if (i == 0)
 		first_child(cmd_lst, fd, path, env_kid, i);
 	else if (i + 1 == len)
-		last_child(cmd_lst, fd, path, env_kid, i, len);
+		last_child(cmd_lst, fd, path, env_kid, i);
 	else
-		mid_kid(cmd_lst, fd, path, env_kid, i, len);
+		mid_kid(cmd_lst, fd, path, env_kid, i);
 }
 
-void	close_all_pipes(t_cmd *cmd_lst, int (*fd)[2])
+void	close_pipes(int (*fd)[2], int i, int len)
 {
-	int	i;
-
-	i = 0;
-	while (cmd_lst != NULL)
-	{
-		close(fd[i][0]);
+	if (i == 0)
 		close(fd[i][1]);
-		i++;
-		cmd_lst = cmd_lst->next;
+	else if ((i + 1) == len)
+		close(fd[i - 1][0]);
+	else
+	{
+		close(fd[i - 1][0]);
+		close(fd[i][1]);
 	}
-	ft_printf("value of i: %d\n", i);
 }
 
 void	exec_alone(t_cmd *cmd_lst, t_env *lst, int *id, char *path)
@@ -157,6 +106,7 @@ void	exec_alone(t_cmd *cmd_lst, t_env *lst, int *id, char *path)
 		perror("fork");
 	else if (id[0] == 0)
 		main_child2(cmd_lst, path, env_kid);
+	free(path);
 }
 
 void	fork_arr(t_env *lst, t_cmd *cmd_lst, int (*fd)[2], int *id)
@@ -178,21 +128,23 @@ void	fork_arr(t_env *lst, t_cmd *cmd_lst, int (*fd)[2], int *id)
 	{
 		while (cmd_lst != NULL)
 		{
-			pipe_arr(cmd_lst, fd);
+			if ((i + 1) != len)
+				pipe_arr(fd, i);
 			id[i] = fork();
 			if (id[i] < 0)
 			{
-				close_all_pipes(cmd_lst, fd);
+				close_pipes(fd, i, len);
 				perror("Fork");
 				return ;
 			}
 			else if (id[i] == 0)
 				check_pos(lst, cmd_lst, fd, len, i);
-			close_all_pipes(cmd_lst, fd);
+			close_pipes(fd, i, len);
 			i++;
 			tmp = cmd_lst;
 			cmd_lst = cmd_lst->next;
 			free(tmp);
+			free(path);
 			if (cmd_lst != NULL)
 			{
 				path = check_access(lst, cmd_lst);
