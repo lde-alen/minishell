eb2cd6c42e4e33d80fbb225fa3ad476ebabcaefa
Alka Santhosh <alka.sv54@gmail.com>
2022-08-30 19:08:47 +0400

    norminetted exit

--- srcs/builtins/ft_exit.c
+++ srcs/builtins/ft_exit.c
@@ -6,7 +6,7 @@
 /*   By: asanthos <asanthos@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/13 03:41:57 by asanthos          #+#    #+#             */
-/*   Updated: 2022/08/30 17:08:00 by asanthos         ###   ########.fr       */
+/*   Updated: 2022/08/30 19:08:23 by asanthos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,7 +38,7 @@ int	check_str_exit(char *arg)
 	while (arg[i])
 	{
 		if (!ft_isdigit(arg[i]))
-			return (1); 
+			return (1);
 		i++;
 	}
 	return (0);
@@ -51,24 +51,33 @@ void	exit_error(t_cmd *cmd_lst)
 	ft_putendl_fd(" numeric argument required", 2);
 }
 
+void	check_valid(t_cmd *cmd_lst)
+{
+	if (((ft_atol(cmd_lst->argument[1]) > 9223372036854775807)
+			&& (cmd_lst->argument[1][0] == '+'
+			|| cmd_lst->argument[1][0] != '-'))
+		|| ((ft_atol(cmd_lst->argument[1])
+			< ft_atol("-9223372036854775808"))
+		&& cmd_lst->argument[1][0] == '-'))
+	{
+		ft_putendl_fd("exit", 1);
+		exit_error(cmd_lst);
+		exit(255);
+	}
+	else
+	{
+		free(cmd_lst->argument[1]);
+		cmd_lst->argument[1] = ft_strdup(ft_ltoa(ft_atol(cmd_lst->argument[1]) % 256));
+	}
+}
+
 void	ft_exit(t_cmd *cmd_lst)
 {
 	if (cmd_lst->argument[1])
 	{
 		if (check_str_exit(cmd_lst->argument[1]) == 0)
-		{ 
-			if (((ft_atol(cmd_lst->argument[1]) > 9223372036854775807) && (cmd_lst->argument[1][0] == '+' || cmd_lst->argument[1][0] != '-'))
-				|| ((ft_atol(cmd_lst->argument[1]) < ft_atol("-9223372036854775808")) && cmd_lst->argument[1][0] == '-'))
-			{
-				ft_putendl_fd("exit", 1);
-				exit_error(cmd_lst);
-				exit(255);
-			}
-			else
-			{
-				free(cmd_lst->argument[1]);
-				cmd_lst->argument[1] = ft_strdup(ft_ltoa(ft_atol(cmd_lst->argument[1]) % 256));
-			}
+		{
+			check_valid(cmd_lst);
 		}
 		else
 		{
