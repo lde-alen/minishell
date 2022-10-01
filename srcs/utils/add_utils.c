4a6e97cf43e6d51ddd9c0c4cd5fd714a59a93288
Alka Santhosh <alka.sv54@gmail.com>
2022-10-01 17:00:09 +0400

    fixed shlvl

--- srcs/utils/add_utils.c
+++ srcs/utils/add_utils.c
@@ -6,7 +6,7 @@
 /*   By: asanthos <asanthos@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/01 12:45:57 by asanthos          #+#    #+#             */
-/*   Updated: 2022/10/01 16:04:42 by asanthos         ###   ########.fr       */
+/*   Updated: 2022/10/01 16:59:25 by asanthos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,28 +17,27 @@ void	set_shlvl(t_lex *lex, t_exec *exec)
 	char	*val;
 	int		shl_val;
 
-	if (exec->path != NULL && ft_strcmp(exec->path, "./minishell")
-		== 0 && search_env(lex->env, "SHLVL") != NULL)
+	if (exec->path != NULL && ft_strcmp(exec->path, "./minishell") == 0
+		&& search_env(lex->env, "SHLVL")->value != NULL)
 	{
 		val = search_env(lex->env, "SHLVL")->value;
-		if (!val)
-			val = ft_strdup("1");
+		shl_val = ft_atoi(val);
+		free(val);
+		if (shl_val < 0)
+			search_env(lex->env, "SHLVL")->value = ft_strdup("0");
+		else if (shl_val < 999)
+			search_env(lex->env, "SHLVL")->value = ft_itoa(shl_val + 1);
+		else if (shl_val == 999)
+			search_env(lex->env, "SHLVL")->value = NULL;
 		else
 		{
-			shl_val = ft_atoi(val);
-			free(val);
-			if (shl_val < 0)
-				val = ft_strdup("0");
-			else if (shl_val < 999)
-				val = ft_itoa(shl_val + 1);
-			else
-			{
-				err_msg(lex->cmd, "warning",
-					"shell level too high, resetting to 1");
-				val = ft_strdup("1");
-			}
+			err_msg(lex->cmd, "warning",
+				"shell level too high, resetting to 1");
+			search_env(lex->env, "SHLVL")->value = ft_strdup("1");
 		}
 	}
+	else if (search_env(lex->env, "SHLVL")->value == NULL)
+		search_env(lex->env, "SHLVL")->value = ft_strdup("1");
 }
 
 t_bool	check_here_doc(t_lex *lex)
